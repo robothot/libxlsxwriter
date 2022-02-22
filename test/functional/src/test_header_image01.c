767ac5311d4d38ec706c0604e20a59cb50f2bac9
John McNamara <jmcnamara@cpan.org>
2022-02-22 14:42:01 +0000

    Fix for potential double free in header/footer.
    Fix for potential double free in header/footer code when
    an error condition occurs.
    
    Issue #366

--- test/functional/src/test_header_image01.c
+++ test/functional/src/test_header_image01.c
@@ -18,5 +18,31 @@ int main() {
 
     worksheet_set_header_opt(worksheet, "&L&G", &header_options);
 
+    /*
+     *  Test error conditions. These functions should be ignored with warnings.
+     */
+
+    /* Test NULL string. */
+    worksheet_set_header(worksheet, NULL);
+    worksheet_set_footer(worksheet, NULL);
+
+
+    /* Test long string. */
+    char *long_string = "This is a long string longer than the Excel limit of 255 characters used for testing                                                                                                                                                                                    end.";
+    worksheet_set_header(worksheet, long_string);
+    worksheet_set_footer(worksheet, long_string);
+
+
+    /* Test &G placeholder without image. */
+    lxw_header_footer_options options2 = {};
+    worksheet_set_header_opt(worksheet, "&L&G", &options2);
+    worksheet_set_footer_opt(worksheet, "&L&G", &options2);
+
+    /* Test too many images/not enough placeholders. */
+    lxw_header_footer_options options3 = {.image_left = "images/red.jpg", .image_right = "images/red.jpg"};
+    worksheet_set_header_opt(worksheet, "&L&G", &options3);
+    worksheet_set_footer_opt(worksheet, "&L&G", &options3);
+
+
     return workbook_close(workbook);
 }
